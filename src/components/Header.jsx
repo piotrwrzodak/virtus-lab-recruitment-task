3ac75cb5b1a33ebdbd9a1cd7bd670f2f55dce336
piotrwrzodak <piotrwrzodakk@gmail.com>
2021-04-03 19:18:48 +0200

    create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="header">
+      <h1 className="header__heading--one">Star Wars</h1>
+      <h3 className="header__heading--two">Character Catalogue</h3>
+    </div>
+  );
+}
+
+export default Header;
